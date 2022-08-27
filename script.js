43a16499ef33d1d7b9c983026e3cf2dd1d0e2882
presh-031 <akingbulugbe.dev@gmail.com>
2022-08-27 23:11:42 +0100

    added temp conversion functionality

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ go.addEventListener("click", getInput);
 //   }
 // });
 
+// radioBtn for temp Conversion
+const radioBtn = document.querySelector("#convert");
+
 function getInput() {
   let userInput = document.querySelector("input").value.toLowerCase();
   console.log(userInput);
@@ -28,6 +31,7 @@ function getLocation(input) {
       // console.log(data);
       console.log(data[0].lat);
       console.log(data[0].lon);
+
       // With the lat and long data that comes back, well call the  getWeatherInfo function
       // This is simply due to how this api works, info which i got to know by READING THE DOCS!
       getWeatherInfo(data[0].lat, data[0].lon);
@@ -53,8 +57,10 @@ function getWeatherInfo(lat, lon) {
       document.querySelector(".humidity").innerHTML = data.main.humidity;
       document.querySelector(".wind").innerHTML = data.wind.speed;
 
-      // calling the convert function after initial results have displayed
-      convert(data.main.temp, data.main.feels_like);
+      // calling the convert function after initial results have displayed and user checks or unchecks the checkbox
+      radioBtn.addEventListener("change", () => {
+        convert(data.main.temp, data.main.feels_like);
+      });
     })
     .catch((err) => {
       console.log(`error ${err}`);
@@ -63,6 +69,19 @@ function getWeatherInfo(lat, lon) {
 
 // Checking the radio should convert to farenheit
 function convert(temp, feelsLike) {
-  console.log(temp, feelsLike);
-  // If radio is checked, convert to farenheit
+  const tempInFarenheit = temp * (9 / 5) + 32;
+  const feelsLikeInFarenheit = feelsLike * (9 / 5) + 32;
+
+  // If checkbox is checked, display farenheit values, else display the original celcius values.
+  if (radioBtn.checked) {
+    document.querySelector(".temp").innerHTML = tempInFarenheit;
+    document.querySelector(".feels-like").innerHTML = feelsLikeInFarenheit;
+    document.querySelector(".unit-1").innerHTML = "&deg;F";
+    document.querySelector(".unit-2").innerHTML = "&deg;F";
+  } else {
+    document.querySelector(".temp").innerHTML = temp;
+    document.querySelector(".feels-like").innerHTML = feelsLike;
+    document.querySelector(".unit-1").innerHTML = "&deg;C";
+    document.querySelector(".unit-2").innerHTML = "&deg;C";
+  }
 }
